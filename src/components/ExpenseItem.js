a45688379fea8360ef3ac5d1284f0e5ab5309b5e
swarajkaran <swarajkaran.raj@gmail.com>
2022-10-07 02:11:05 +0530

    ExpenseItem init component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+      </div>
+      <div>Amount</div>
+    </>
+  );
+}
+export default ExpenseItem;
